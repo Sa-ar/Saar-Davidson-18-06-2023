e14deaac90c9699050c0972fe4c60292e4ecfc57
Saar Davidson <davidsonsaar@gmail.com>
2023-06-23 17:03:48 +0300

    add persistent to redux

--- src/store.ts
+++ src/store.ts
@@ -1,14 +1,22 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { useDispatch, useSelector } from 'react-redux'
-import type { TypedUseSelectorHook } from 'react-redux'
+import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { useDispatch, useSelector, TypedUseSelectorHook } from 'react-redux'
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { weatherApi } from '@/feature/weather'
-import selectedCityReducer from '@/feature/locationsSlice'
+import selectedCityReducer from '@/feature/locationsSlice';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, combineReducers({
+  [weatherApi.reducerPath]: weatherApi.reducer,
+  selectedCity: selectedCityReducer,
+}))
 
 export const store = configureStore({
-  reducer: {
-    [weatherApi.reducerPath]: weatherApi.reducer,
-    selectedCity: selectedCityReducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(weatherApi.middleware),
 })
@@ -19,3 +27,4 @@ export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export const useAppDispatch: () => AppDispatch = useDispatch
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
+export const persister = persistStore(store)
